af8960569f773c536dfd6c0358df24184dae28fd
Benedikt Reinartz <filmor@gmail.com>
2022-08-06 18:27:16 +0200

    Fix fish config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -5,8 +5,14 @@ end
 function fish_greeting
 end
 
+if type -q starship
+    source (starship init fish --print-full-init | psub)
+end
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /home/u37166/mambaforge/bin/conda "shell.fish" "hook" $argv | source
+if test -f ~/mambaforge/bin/conda
+    eval ~/mambaforge/bin/conda "shell.fish" "hook" $argv | source
+end
 # <<< conda initialize <<<
 
