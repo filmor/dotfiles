dc255c932b841a0cc5712a2cd8d3a35fdb7f022d
Benedikt Reinartz <filmor@gmail.com>
2025-01-08 15:04:16 +0100

    Switch to LazyNvim

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,27 @@
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.textwidth = 80
+vim.opt.tabstop = 4
+vim.opt.encoding = "utf-8"
+vim.opt.expandtab = true
+vim.opt.visualbell = true
+vim.opt.ffs = "unix,dos"
+vim.opt.mouse = "a"
+vim.opt.colorcolumn = "80"
+
+vim.opt.foldenable = false
+
+if vim.g.neovide then
+  vim.opt.guifont = "FiraCode Nerd Font,Fira Code:h11"
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+end
+
+-- Don't start comment when creating a new line with 'o' or 'O'
+vim.opt.formatoptions:remove("o")
+vim.opt.formatoptions:remove("t")
+
+vim.opt.switchbuf:append("useopen,usetab")
+vim.opt.splitright = true
