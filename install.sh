277bfe40f1cb846617d11dc561a409db769f846f
Benedikt Reinartz <filmor@gmail.com>
2025-02-21 09:05:36 +0100

    Use one-shot install on Coder

--- install.sh
+++ install.sh
@@ -23,7 +23,11 @@ fi
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-set -- init --apply --source="${script_dir}"
+if [ "x$CODER" == "xtrue" ]; then
+	set -- init --one-shot --source="${script_dir}"
+else
+	set -- init --apply --source="${script_dir}"
+fi
 
 echo "Running 'chezmoi $*'" >&2
 # exec: replace current process with chezmoi
