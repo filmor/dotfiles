96813174fa51a871f87af4345b562878763e3c03
Benedikt Reinartz <filmor@gmail.com>
2025-04-11 18:31:19 +0200

    Guard atuin fish config

--- dot_config/fish/conf.d/atuin.env.fish
+++ dot_config/fish/conf.d/atuin.env.fish
@@ -1,2 +1,8 @@
+set local_path "$HOME/.atuin/bin/env.fish"
+set gentoo_path "/usr/share/atuin/shell-init/fish"
 
-source "$HOME/.atuin/bin/env.fish"
+if test -e $local_path
+    source $local_path
+else if test -e $gentoo_path
+    source $gentoo_path
+end
