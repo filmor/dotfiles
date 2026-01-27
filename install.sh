ad9f59829e3d925cbfad8845936b0136c420a9a9
Benedikt Reinartz <filmor@gmail.com>
2026-01-27 08:07:34 +0100

    Fall back to stored chezmoi install script to only depend on github

--- install.sh
+++ install.sh
@@ -9,15 +9,24 @@ if ! chezmoi="$(command -v chezmoi)"; then
 	chezmoi="${bin_dir}/chezmoi"
 	echo "Installing chezmoi to '${chezmoi}'" >&2
 	if command -v curl >/dev/null; then
-		chezmoi_install_script="$(curl -fsSL https://get.chezmoi.io)"
+		download_cmd="curl -fsSL https://get.chezmoi.io"
 	elif command -v wget >/dev/null; then
-		chezmoi_install_script="$(wget -qO- https://get.chezmoi.io)"
+		download_cmd="wget -qO- https://get.chezmoi.io"
 	else
 		echo "To install chezmoi, you must have curl or wget installed." >&2
 		exit 1
 	fi
+	if ! chezmoi_install_script="$(${download_cmd})"; then
+		echo "Failed to download chezmoi install script, falling back to local chezmoi-install.sh" >&2
+		if [ -f "./chezmoi-install.sh" ]; then
+			chezmoi_install_script="$(cat "$(dirname "$0")/chezmoi-install.sh")"
+		else
+			echo "Local chezmoi-install.sh not found." >&2
+			exit 1
+		fi
+	fi
 	sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
-	unset chezmoi_install_script bin_dir
+	unset chezmoi_install_script bin_dir download_cmd
 fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
